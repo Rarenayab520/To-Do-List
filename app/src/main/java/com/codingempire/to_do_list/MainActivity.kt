08a7610f4056d24b7bda3824f902e93947984593
Rarenayab520 <nayabasmat121@gmail.com>
2025-09-03 12:01:07 +0100

    LOGIN backend Done

--- app/src/main/java/com/codingempire/to_do_list/MainActivity.kt
+++ app/src/main/java/com/codingempire/to_do_list/MainActivity.kt
@@ -5,11 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.codingempire.to_do_list.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-       
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            navController = navHostFragment.navController
+
+        if (auth.currentUser != null) {
+            navController.navigate(R.id.homeFragment)
+        } else {
+            navController.navigate(R.id.loginFragment)
+        }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
